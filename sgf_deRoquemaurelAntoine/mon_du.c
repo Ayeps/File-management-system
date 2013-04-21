44237e0951b523ad9e42e0141aab42e77882a66b
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-04-21 15:13:50 +0200

    Finalisation dépot / rapport

---
+++ sgf_deRoquemaurelAntoine/mon_du.c
@@ -0,0 +1,89 @@
+/********************************************************************************************/
+/* mon_du : Affiche la sommes des éléments du répertoire passé en paramètre					*/
+/*																							*/
+/* C. Collet 2013 ® Université Paul Sabatier, Toulouse										*/
+/********************************************************************************************/
+
+#include "mon_SGF.h"
+
+int tailleSousRep(int inoeud, SGF_t* mon_SGF);
+int tailleTotalRepertoire(char* chemin, SGF_t* mon_SGF);
+int main(int argc, char * argv[])
+{
+    SGF_t * mon_SGF;		/* ma structure de SGF */
+    int inoeud_rep;
+	char* chemin;
+    dir_element_t * liste_rep = NULL;
+    int i;
+	int list=0;
+
+    if (argc != 2) {
+		fprintf(stderr,"Usage: %s dir\n", argv[0]);
+		exit (EXIT_PARAM);
+    } else {
+		chemin = argv[1];
+	}
+
+    if (chemin[0]!='/') {
+		fprintf(stderr,"%s: erreur de designation \"%s\" doit commencer par '/' (chemin absolu).\n", argv[0], argv[1]);
+		exit (EXIT_PARAM);
+    }
+
+    mon_SGF = ouvrir_SGF();
+    if (mon_SGF == NULL ) {
+		fprintf(stderr,"%s: ", argv[0]);
+		perror (DEVICE_NAME);
+		exit (EXIT_DEVICE);
+    }
+
+	printf("%d o", tailleTotalRepertoire(chemin, mon_SGF));
+    mon_SGF=fermer_SGF(mon_SGF);
+
+    exit (0);
+}
+
+int tailleSousRep(int inoeud, SGF_t* mon_SGF) {
+	return mon_SGF->table_inoeuds[inoeud].taille;
+}
+
+int tailleTotalRepertoire(char* chemin, SGF_t* mon_SGF) {
+    int inoeud_rep=inoeud_designation_element(chemin, 0, mon_SGF);
+	int i;
+    dir_element_t * liste_rep = NULL;
+	int taille = 0;
+	char sousChemin[256];
+
+    if (inoeud_rep == NO_INOEUD) {
+		fprintf(stderr,"%s: chemin \"%s\" inexistant !\n", "mon_du", chemin);
+		exit (EXIT_PARAM);
+    }
+
+    if ((liste_rep =lire_liste_rep_dans_inoeud ( inoeud_rep, mon_SGF)) == NULL) {
+		affiche_erreur( EXIT_DEVICE , "mon_du");
+		exit (EXIT_DEVICE);
+    }
+
+	taille += tailleSousRep(inoeud_rep, mon_SGF);
+	if((strcmp(liste_rep[1].elt_name, "..") == 0 && liste_rep[2].elt_inoeud != NO_INOEUD) ||
+			(liste_rep[1].elt_inoeud != NO_INOEUD && strcmp(liste_rep[1].elt_name, "..") != 0)) {
+		/* Si le dossier est / il n'y a pas de .. */
+		if(strcmp(chemin, "/") == 0) {
+		  i=1;
+		} else {
+		  i=2;
+		}
+
+		while(liste_rep[i].elt_inoeud != NO_INOEUD) {
+			strcpy(sousChemin, chemin);
+			if(strcmp(chemin, "/") != 0) {
+				strcat(sousChemin, "/");
+			}
+			strcat(sousChemin, liste_rep[i].elt_name);
+			taille += tailleTotalRepertoire(sousChemin, mon_SGF);
+			strcpy(sousChemin, "");	
+			++i;
+		}
+	}
+
+	return taille;
+}
---
+++ sgf_deRoquemaurelAntoine/mon_ls.c
@@ -0,0 +1,86 @@
+/************************************************************************************************************************/
+/* mon_ls : affiche le contenu du répertoire passé en paramètre. Le chemin doit obligatoirement être absolu.		*/
+/*															*/
+/* C. Collet 2013 ® Université Paul Sabatier, Toulouse									*/
+/************************************************************************************************************************/
+
+#include "mon_SGF.h"
+
+void afficherInoeud(int inoeud, char* chemin, int taille);
+int main(int argc, char * argv[])
+{
+    SGF_t * mon_SGF;		/* ma structure de SGF */
+    int inoeud_rep;
+	char* chemin;
+    dir_element_t * liste_rep = NULL;
+    int i;
+	int list=0;
+
+    if ( argc != 2 && argc != 3)
+    {
+		fprintf(stderr,"Usage: %s [-l] dir\n", argv[0]);
+		exit (EXIT_PARAM);
+    }
+	if(argc == 3) {
+		if(strcmp(argv[1], "-l") != 0) {
+			fprintf(stderr,"Usage: %s [-l] dir\n", argv[0]);
+			exit (EXIT_PARAM);
+		}
+		chemin = argv[2];
+		list = 1;
+	} else {
+		chemin = argv[1];
+	}
+
+    if (chemin[0]!='/')
+    {
+	fprintf(stderr,"%s: erreur de designation \"%s\" doit commencer par '/' (chemin absolu).\n", argv[0], argv[1]);
+	exit (EXIT_PARAM);
+    }
+
+    mon_SGF = ouvrir_SGF();
+    if (mon_SGF == NULL )
+    {
+	fprintf(stderr,"%s: ", argv[0]);
+	perror (DEVICE_NAME);
+	exit (EXIT_DEVICE);
+    }
+
+    inoeud_rep=inoeud_designation_element(chemin, 0, mon_SGF);
+
+    if (inoeud_rep == NO_INOEUD)
+    {
+	fprintf(stderr,"%s: chemin \"%s\" inexistant !\n", argv[0], chemin);
+	exit (EXIT_PARAM);
+    }
+
+    if ((liste_rep =lire_liste_rep_dans_inoeud ( inoeud_rep, mon_SGF)) == NULL)
+    {
+	affiche_erreur( EXIT_DEVICE , argv[0]);
+	exit (EXIT_DEVICE);
+    }
+
+	afficherInoeud(inoeud_rep, chemin, (list) ? mon_SGF->table_inoeuds[inoeud_rep].taille : -1);
+    i=0;
+    while( liste_rep[i].elt_inoeud != NO_INOEUD)
+    {
+		afficherInoeud(liste_rep[i].elt_inoeud, liste_rep[i].elt_name, (list) ? mon_SGF->table_inoeuds[liste_rep[i].elt_inoeud].taille : -1);
+		i++;
+    }
+
+    mon_SGF=fermer_SGF(mon_SGF);
+
+    exit (0);
+}
+
+/* 
+ * Afficher les informations d'un inoeud
+ * @param taille La taille du fichier, si celle-ci est -1, la taille ne sera pas affiché.
+ */
+void afficherInoeud(int numInoeud, char* chemin, int taille) {
+	if(taille == -1) {
+		printf("%3d '%s' :\n", numInoeud, chemin);
+	} else {
+		printf("%3d\t %4d o \t'%s' :\n", numInoeud, taille, chemin);
+	}
+}
